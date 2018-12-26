9e6e80898e813de783f032bc2b48fda66f7ab70c
jerryliu0811 <jerryliu0811@gmail.com>
2018-12-26 23:15:36 +0800

    minor change ui for approved_table

--- web/js/approved.js
+++ web/js/approved.js
@@ -1,12 +1,15 @@
 function render_approved_div(class_item, approved_list){
     var approved_table_str = '<table class="table table-hover">';
+    approved_table_str += "<tr><th width='40%'>名字</th><th width='50%'>敘述</th><th width='10%'></th></tr>"
     approved_list.forEach((approved_item) => {
         var id = approved_item.id,
-            info = approved_item.name;
+            name = approved_item.name,
+            description = approved_item.description;
 
         approved_table_str += '\
             <tr id="' + id + '">\
-                <td>' + info + '</td>\
+                <td>' + name + '</td>\
+                <td>' + description + '</td>\
                 <td><button id="' + id + '_approvedbtn" class="btn btn-secondary approvedbtn">編輯</button></td>\
             </tr>';
     });
@@ -17,7 +20,7 @@ function render_approved_div(class_item, approved_list){
         <br>\
         <h3 class="center">請點選欲編輯的' + class_item.name + '檔案</h3>\
         <br>\
-        <div class="margin_center approved_table">\
+        <div class="margin_table approved_table">\
         ' + approved_table_str + ' \
         </div>\
         ';
